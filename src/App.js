302b20a08d86cad33ab372a0e908249a78587eab
Mucyosoda <mucyosofa@gmail.com>
2021-11-02 08:29:24 +0100

    use react-redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <h1>Math Magician</h1>
       </header>
     </div>
