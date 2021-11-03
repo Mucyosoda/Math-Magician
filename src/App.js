545f04ad8487d226d583d62d845d61e4f1fccb41
Mucyosoda <mucyosofa@gmail.com>
2021-11-03 05:43:20 +0100

    add classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Calc from './Components/calculator';
-
-const App = () => (
-  <div>
-    <Calc />
-  </div>
-);
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calc />
+      </div>
+    );
+  }
+}
 
 export default App;
