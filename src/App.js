9a133fc1bc085b26e4ed7cd7e85bc432e13326cd
Mucyosoda <mucyosofa@gmail.com>
2021-11-04 21:11:23 +0100

    disable import/extension

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+// eslint-disable-next-line import/extensions
 import DoCalculation from './components/calculator';
 import './App.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <DoCalculation />
       </div>
     );
