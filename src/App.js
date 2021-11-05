08dcf7089fc3edcbd5b606bc09ecb5d7ce78f2ec
Mucyosoda <mucyosofa@gmail.com>
2021-11-05 08:41:21 +0100

    Change to function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DoCalculation from './components/calculator';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <DoCalculation />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <DoCalculation />
+  </div>
+);
 
 export default App;
