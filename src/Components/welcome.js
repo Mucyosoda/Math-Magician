95d17989ce11e26a8313b3ed314803aa15dbaa65
Mucyosoda <mucyosofa@gmail.com>
2021-11-12 12:49:39 +0100

    Delete files

--- src/Components/welcome.js
+++ src/Components/welcome.js
@@ -3,7 +3,7 @@ import styles from './Welcome.css';
 
 const Home = () => (
   <div className={styles.header}>
-    <h1> A paragraph is a series of sentencessss</h1>
+    <h1> A paragraph is a series of sentences</h1>
     <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry standard dummy text ever since the
